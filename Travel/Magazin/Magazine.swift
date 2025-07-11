e88db3768ec6208e72770b23bd15703f4aeb148d
Kondol <kyk9702@gmail.com>
2025-07-11 20:00:25 +0900

    init

---
+++ Travel/Magazin/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Travel
+//
+//  Created by yk on 7/11/25.
+//
+
+import Foundation
+
+struct Magazine {
+    var title: String
+    var subtitle: String
+    var photo_image: String
+    var date: String
+    var link: String?
+}
---
+++ Travel/RealTravel/Travel.swift
@@ -0,0 +1,16 @@
+//
+//  Travel.swift
+//  Travel
+//
+//  Created by yk on 7/11/25.
+//
+
+struct Travel {
+    var title: String
+    var description: String?
+    var travel_image: String?
+    var grade: Double?
+    var save: Int?
+    var like: Bool?
+    var ad: Bool?
+}
