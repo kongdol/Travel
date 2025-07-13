e933a6984671c534baf3db31e5a740f7264092ed
Kondol <kyk9702@gmail.com>
2025-07-13 15:54:09 +0900

    여행매거진화면완성

--- Travel/Magazin/MagazineTableViewController.swift
+++ Travel/Magazin/MagazineTableViewController.swift
@@ -27,11 +27,37 @@ class MagazineTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "magazineCell", for: indexPath) as! MagazineTableViewCell
         
+        //기본정보
         cell.magazineTitle.text = mz.magazine[indexPath.row].title
+        cell.magazineTitle.font = UIFont.boldSystemFont(ofSize: 20)
+        
         cell.magazineSubtitle.text = mz.magazine[indexPath.row].subtitle
+        cell.magazineSubtitle.font = UIFont.systemFont(ofSize: 12)
+        cell.magazineSubtitle.textColor = .lightGray
+        
+        // 이미지
         let url = URL(string: mz.magazine[indexPath.row].photo_image)
         cell.magazineImage.kf.setImage(with: url)
-        cell.magazineDate.text = mz.magazine[indexPath.row].date
+        cell.magazineImage.layer.cornerRadius = 8
+        
+    
+        // 날짜
+        let format = DateFormatter()
+        format.dateFormat = "yyMMdd"
+        let a = mz.magazine[indexPath.row].date
+        
+        if let dateResult = format.date(from: a) {
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = "yy년MM월dd일"
+            let outputString = outputFormatter.string(from: dateResult)
+            cell.magazineDate.text = outputString
+        } else {
+            cell.magazineDate.text = "25년07월14일"
+            print("날짜에 기본값이 들어갔어요!")
+        }
+        
+        cell.magazineDate.font = UIFont.systemFont(ofSize: 12)
+        cell.magazineDate.textColor = .lightGray
         
         return cell
     }
